977bf008c85a5b7d7ef6f3cf7f1745a7678b217e
veertikothari <58428145+veertikothari@users.noreply.github.com>
2020-01-28 21:22:46 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# raindrops
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,49 @@
+function setup() {
+  createCanvas(800,700);
+
+  createSprite(100, 10, 5, 50);
+  createSprite(150, 50, 5, 50);
+  createSprite(295, 690, 5, 50);
+  createSprite(250, 620, 5, 50);
+  createSprite(325, 600, 5, 50);
+  createSprite(360, 650, 5, 50);
+  createSprite(485, 670, 5, 50);
+  createSprite(460, 630, 5, 50);
+  createSprite(530, 580, 5, 50);
+  createSprite(550, 550, 5, 50);
+  createSprite(690, 420, 5, 50);
+  createSprite(630, 450, 5, 50);
+  createSprite(720, 390, 5, 50);
+  createSprite(750, 350, 5, 50);
+  createSprite(810, 200, 5, 50);
+  createSprite(10, 210, 5, 50);
+  createSprite(50, 170, 5, 50);
+  createSprite(70, 150, 5, 50);
+  createSprite(150, 395, 5, 50);
+  createSprite(190, 590, 5, 50);
+  createSprite(285, 455, 5, 50);
+  createSprite(745, 735, 5, 50);
+  createSprite(665, 295, 5, 50);
+  createSprite(555, 115, 5, 50);
+  createSprite(225, 490, 5, 50);
+  createSprite(125, 582, 5, 50);
+  createSprite(15,10 , 5, 50);
+  createSprite(23, 36, 5, 50);
+  createSprite(677, 75,5, 50);
+  createSprite(150, 697, 5, 50);
+  createSprite(147, 326, 5, 50);
+  createSprite(150, 250, 5, 50);
+  createSprite(326, 17, 5, 50);
+  createSprite(17, 417, 5, 50);
+  createSprite(123, 23, 5, 50);
+  createSprite(188, 677, 5, 50);
+  createSprite(697, 36, 5, 50);
+  createSprite(75, 529, 5, 50);
+  createSprite(36, 40, 5, 50);
+}
+
+function draw() {
+  background("purple");  
+  drawSprites();
+  friction=0.9;
+}
